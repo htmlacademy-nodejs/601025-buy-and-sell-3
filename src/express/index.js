a16a8eb3e7e85e1e6bcdee0fe875fec78b4670cb
Inspel <bozhan89@gmail.com>
2020-02-29 22:19:29 +0300

    15. Учебный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const express = require(`express`);
 const homeRoute = require('./routes/homeRoute');
 const searchRoutes = require(`./routes/search`);
@@ -7,9 +8,16 @@ const myRoutes = require(`./routes/my`);
 const loginRoutes = require(`./routes/login`);
 const offersRoutes = require(`./routes/offers`);
 const registerRoutes = require(`./routes/register`);
+const errorsRoutes = require(`./routes/errors`);
+
+const STATIC_DIR = path.join(__dirname, `./../../markup`);
 
 const app = express();
 const port = 8080;
+
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.join(__dirname, `./templates`));
 
 app.use(`/`, homeRoute);
 app.use(`/login`, loginRoutes);
@@ -17,6 +25,9 @@ app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 app.use(`/register`, registerRoutes);
 app.use(`/search`, searchRoutes);
+app.use(`/errors`, errorsRoutes);
+
+app.use(express.static(STATIC_DIR));
 
 
 app.listen(port);
--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/`, (req, res) => res.send(`/offers`));
-offersRouter.get(`/:id`, (req, res) => res.send(`/offers/${req.params.id}`));
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/${req.params.id}`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/${req.params.id}`));
+offersRouter.get(`/add`, (req, res) => res.render(`pages/new-ticket.pug`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`pages/ticket-edit.pug`));
+offersRouter.get(`/category/:id`, (req, res) => res.render('pages/category.pug'));
+offersRouter.get(`/:id`, (req, res) => res.render(`pages/ticket`));
 
 module.exports = offersRouter;
